b79c6716d37ab6ee1aa139dc2cde6e43d42c4be2
MuhammadJunaid01 <m.junaidbkh2020@gmail.com>
2024-05-18 00:30:29 +0600

    declare globale type

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import app from "./app";
 import mongoose from "mongoose";
 import config from "./app/config";
+console.log(process.env.NODE_ENV);
 const main = async () => {
   try {
     const dbUri = config.database_url;
